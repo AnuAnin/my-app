aaf8b30ce96afeab16a077d7f427880a9d7e66da
anubhav <you@example.com>
2025-07-19 08:01:57 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+# Import standard libraries
+import sys
+import os
+import datetime
+
+# Print Hello World
+print("Hello, World!")
+
+# Use imported libraries (just basic usage examples)
+print("Current Python version:", sys.version)
+print("Current working directory:", os.getcwd())
+print("Current date and time:", datetime.datetime.now())
